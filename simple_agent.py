7feb5241bb3d61b37fd1b49549e45e471671c39a
Kevin <rocketlikeabass@gmail.com>
2018-05-01 23:51:08 -0600

    build spawning pool

--- simple_agent.py
+++ simple_agent.py
@@ -20,6 +20,7 @@ _ZERG_DRONE = 104
 _ZERG_ZERGLING = 105
 _ZERG_LARVA = 151
 _ZERG_HATCHERY = 86
+_ZERG_SPAWNINGPOOL = 89
 
 # Parameters
 _PLAYER_SELF = 1
@@ -32,10 +33,10 @@ class SimpleAgent(base_agent.BaseAgent):
     drone_selected = False
     larva_selected = False;
     hatchery_selected = False
-    spawningPool_built = False
+    spawningpool_built = False
     
     
-    def transformLocation(self, x, x_distance, y, y_distance):
+    def transform_location(self, x, x_distance, y, y_distance):
         if not self.base_top_left:
             return [x - x_distance, y - y_distance]
     
@@ -44,7 +45,7 @@ class SimpleAgent(base_agent.BaseAgent):
     def step(self, obs):
         super(SimpleAgent, self).step(obs)
         
-        time.sleep(0.5)
+        time.sleep(0.2)
         
         if self.base_top_left is None:
             player_y, player_x = (obs.observation["minimap"][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
@@ -61,7 +62,27 @@ class SimpleAgent(base_agent.BaseAgent):
                 
                 return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
             elif _SPAWN_OVERLORD in obs.observation["available_actions"]:
+                self.overlord_spawned = True
+
                 return actions.FunctionCall(_SPAWN_OVERLORD, [_QUEUED])
-            
+        elif not self.spawningpool_built:
+            if not self.drone_selected:
+                unit_type = obs.observation["screen"][_UNIT_TYPE]
+                unit_y, unit_x = (unit_type == _ZERG_DRONE).nonzero()
+
+                target = [unit_x[0], unit_y[0]]
+
+                self.drone_selected = True
+
+                return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
+            elif _BUILD_SPAWNING_POOL in obs.observation["available_actions"]:
+                unit_type = obs.observation["screen"][_UNIT_TYPE]
+                unit_y, unit_x = (unit_type == _ZERG_HATCHERY).nonzero()
+
+                target = self.transform_location(int(unit_x.mean()), 0, int(unit_y.mean()), 20)
+
+                self.spawningpool_built = True
+
+                return actions.FunctionCall(_BUILD_SPAWNING_POOL, [_NOT_QUEUED, target])
         
         return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
